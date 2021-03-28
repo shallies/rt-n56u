f1da499f23e38c10c6ba2ed17316e2e83a792fdc
shallies <81499972+shallies@users.noreply.github.com>
2021-03-28 16:35:53 +0800

    Update vlmcsd.sh

--- trunk/user/vlmcsd/vlmcsd.sh
+++ trunk/user/vlmcsd/vlmcsd.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
 func_start(){
-	vlmcsd
-	logger -st "vlmcsd" "start"
+        #Check internet connection
+        n=30
+        while [ $n -gt 0 ]; do
+                ping -c 1 -W 1 114.114.114.114 >/dev/null
+                if [ $? = 0 ]; then
+                        break;
+                fi;
+                n=$((n-1))
+                sleep 1
+        done
+
+        #Synchronize clock
+        ntpd -nq -p ntp1.aliyun.com
+        if [ $? != 0 ]; then
+                ntpd -nq -p ntp.aliyun.com
+        fi
+
+        vlmcsd -l /tmp/vlmcsd.log
+        logger -st "vlmcsd" "Start"
 }
 
 func_stop(){
