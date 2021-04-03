6a28d8986d274a55a051fda67e1a5903c32d4e5d
shallies <81499972+shallies@users.noreply.github.com>
2021-04-03 11:24:46 +0800

    Update autostart.sh

--- trunk/user/scripts/autostart.sh
+++ trunk/user/scripts/autostart.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 #nvram set ntp_ready=0
 if [ $(nvram get sdns_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动SmartDns"
+logger -t "自动启动" "正在启动SmartDns ..."
 /usr/bin/smartdns.sh start
 fi
 
 if [ $(nvram get caddy_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动文件管理"
+logger -t "自动启动" "正在启动文件管理 ..."
 /usr/bin/caddy.sh start
 fi
 
-logger -t "自动启动" "正在检查Internet连接状态！"
+logger -t "自动启动" "正在检查Internet连接状态 ..."
 count=60
 while [ $count -gt 0 ]; do
 	ping -c 1 -W 1 114.114.114.114 >/dev/null
 	if [ $? = 0 ]; then
-		logger -t "自动启动" "Internet已连接！"
+		logger -t "自动启动" "Internet已连接，正在同步时钟 ..."
+		ntpd -nq -p 120.25.115.20  #ntp1.aliyun.com
+		if [ $? != 0 ]; then
+			ntpd -nq -p 203.107.6.88  #ntp2.aliyun.com
+		fi
 		break;
 	fi;
 	count=$((count-1))
@@ -23,41 +27,41 @@ while [ $count -gt 0 ]; do
 done
 
 if [ $(nvram get vlmcsd_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动vlmcsd"
+logger -t "自动启动" "正在启动vlmcsd ..."
 /usr/bin/vlmcsd.sh start
 fi
 
 if [ $(nvram get adbyby_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动adbyby plus+"
+logger -t "自动启动" "正在启动adbyby plus+ ..."
 /usr/bin/adbyby.sh start
 fi
 
 if [ $(nvram get koolproxy_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动koolproxy"
+logger -t "自动启动" "正在启动koolproxy ..."
 /usr/bin/koolproxy.sh start
 fi
 
 if [ $(nvram get aliddns_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动阿里ddns"
+logger -t "自动启动" "正在启动阿里ddns ..."
 /usr/bin/aliddns.sh start
 fi
 
 if [ $(nvram get ss_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动科学上网"
+logger -t "自动启动" "正在启动科学上网 ..."
 /usr/bin/shadowsocks.sh start
 fi
 
 if [ $(nvram get adg_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动adguardhome"
+logger -t "自动启动" "正在启动adguardhome ..."
 /usr/bin/adguardhome.sh start
 fi
 
 if [ $(nvram get wyy_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动音乐解锁"
+logger -t "自动启动" "正在启动音乐解锁 .."
 /usr/bin/unblockmusic.sh start
 fi
 
 if [ $(nvram get zerotier_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动zerotier"
+logger -t "自动启动" "正在启动zerotier ..."
 /usr/bin/zerotier.sh start
 fi
