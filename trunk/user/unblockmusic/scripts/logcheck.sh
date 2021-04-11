3d30aab3e2606f6f72f259a15dbbe9bcd0e8cdea
Shallies <81499972+shallies@users.noreply.github.com>
2021-04-11 18:12:26 +0800

    Update logcheck.sh

--- trunk/user/unblockmusic/scripts/logcheck.sh
+++ trunk/user/unblockmusic/scripts/logcheck.sh
@@ -9,12 +9,10 @@ sleep 29s
 
 while true
 do
-	icount=`busybox ps -w | grep UnblockNeteaseMusic | grep -v grep | grep -v logcheck.sh`
+	icount=`busybox ps -w | grep [U]nblockNeteaseMusic | grep -v logcheck.sh`
 	if [ -z "$icount" ]; then
 		/usr/bin/getmusicip.sh
  		/usr/bin/unblockmusic restart 
-	else
-		exit
 	fi
 #	log_size=$(expr $(ls -l $log_file | awk '{print $5}') / 1024)
 #	[ $log_size -ge $log_max_size ] && echo "$(date -R) # Start UnblockNeteaseMusic" >/tmp/unblockmusic.log
