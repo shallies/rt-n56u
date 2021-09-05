1d0056c5090fe2df5bb824f6a0f130b8c9d290c8
Shallies <81499972+shallies@users.noreply.github.com>
2021-09-05 17:46:58 +0800

    Update autostart.sh
    Don't check internet status and don't launch vlmcsd.sh  in autostart.sh

--- trunk/user/scripts/autostart.sh
+++ trunk/user/scripts/autostart.sh
@@ -10,26 +10,22 @@ logger -t "自动启动" "正在启动文件管理 ..."
 /usr/bin/caddy.sh start
 fi
 
-logger -t "自动启动" "正在检查Internet连接状态 ..."
-count=60
-while [ $count -gt 0 ]; do
-	ping -c 1 -W 1 114.114.114.114 >/dev/null
-	if [ $? = 0 ]; then
-		logger -t "自动启动" "Internet已连接，正在同步时钟 ..."
-		ntpd -nq -p 120.25.115.20  #ntp1.aliyun.com
-		if [ $? != 0 ]; then
-			ntpd -nq -p 203.107.6.88  #ntp2.aliyun.com
-		fi
-		break;
-	fi;
-	count=$((count-1))
-	sleep 1
-done
+#logger -t "自动启动" "正在检查Internet连接状态 ..."
+#count=60
+#while [ $count -gt 0 ]; do
+#	ping -c 1 -W 1 114.114.114.114 >/dev/null
+#	if [ $? = 0 ]; then
+#		logger -t "自动启动" "Internet已连接，正在同步时钟 ..."
+#		ntpd -nq -p 120.25.115.20  #ntp1.aliyun.com
+#		if [ $? != 0 ]; then
+#			ntpd -nq -p 203.107.6.88  #ntp2.aliyun.com
+#		fi
+#		break;
+#	fi;
+#	count=$((count-1))
+#	sleep 1
+#done
 
-if [ $(nvram get vlmcsd_enable) = 1 ] ; then
-logger -t "自动启动" "正在启动 vlmcsd ..."
-/usr/bin/vlmcsd.sh start
-fi
 
 if [ $(nvram get adbyby_enable) = 1 ] ; then
 logger -t "自动启动" "正在启动 adbyby plus+ ..."
