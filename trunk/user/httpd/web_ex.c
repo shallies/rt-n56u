2bee64e05f903f47560cefce86984da55fdafcb2
Shallies <81499972+shallies@users.noreply.github.com>
2026-07-25 18:02:44 +0800

    Add mime handler for custom folders

--- trunk/user/httpd/web_ex.c
+++ trunk/user/httpd/web_ex.c
@@ -4189,6 +4189,9 @@ do_kp_crt_file(const char *url, FILE *stream)
 #endif
 
 struct mime_handler mime_handlers[] = {
+	/* No need to authenticate for custom folders */
+	{ "custom/**", "text/plain", no_cache_IE, NULL, do_file, 0 }, // Custom folders
+
 	/* cached javascript files w/o translations */
 	{ "jquery.js", "text/javascript", NULL, NULL, do_file, 0 }, // 2012.06 Eagle23
 	{ "**bootstrap.min.js", "text/javascript", NULL, NULL, do_file, 0 }, // 2012.06 Eagle23
